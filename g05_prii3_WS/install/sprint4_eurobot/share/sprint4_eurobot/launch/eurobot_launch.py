66c3e144ea86ec59da4abfde7a05ed3a0c1a3df4
Tu Nombre <rbriram@upv.edu.es>
2025-12-04 12:08:14 +0100

    Textura del circuito añadida

--- g05_prii3_WS/install/sprint4_eurobot/share/sprint4_eurobot/launch/eurobot_launch.py
+++ g05_prii3_WS/install/sprint4_eurobot/share/sprint4_eurobot/launch/eurobot_launch.py
@@ -2,12 +2,21 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
-from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     package_name = 'sprint4_eurobot'
     pkg_share = get_package_share_directory(package_name)
     world_file = os.path.join(pkg_share, 'world', 'eurobot.world')
+
+    # --- CORRECCIÓN IMPORTANTE ---
+    # Si la variable ya existe, añadimos nuestra ruta al final.
+    if 'GAZEBO_RESOURCE_PATH' in os.environ:
+        os.environ['GAZEBO_RESOURCE_PATH'] = os.environ['GAZEBO_RESOURCE_PATH'] + ':' + pkg_share
+    else:
+        # SI NO EXISTE, debemos incluir la ruta por defecto de Gazebo (/usr/share/gazebo-11)
+        # Si no hacemos esto, Gazebo no encuentra sus shaders y se cierra.
+        os.environ['GAZEBO_RESOURCE_PATH'] = '/usr/share/gazebo-11:' + pkg_share
+    # -----------------------------
 
     return LaunchDescription([
         ExecuteProcess(
--- g05_prii3_WS/src/sprint4_eurobot/setup.py
+++ g05_prii3_WS/src/sprint4_eurobot/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'sprint4_eurobot'
 
@@ -10,14 +12,21 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/eurobot_launch.py']),
-        ('share/' + package_name + '/world', ['world/eurobot.world']),
+        
+        # Archivos Launch y World
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'world'), glob('world/*.world')),
+        
+        # --- NUEVO: Instalar texturas y scripts de materiales ---
+        (os.path.join('share', package_name, 'materials/scripts'), glob('materials/scripts/*')),
+        (os.path.join('share', package_name, 'materials/textures'), glob('materials/textures/*')),
+        # --------------------------------------------------------
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='rodrigo',
     maintainer_email='rbriram@upv.edu.es',
-    description='TODO: Package description',
+    description='Sprint 4 Eurobot - Recreación y Visión',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
